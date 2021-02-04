b0faa155ac425e1635cf4b3a02930c7751997024
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-02-03 20:40:35 -0800

    page chosen, .py imports working, content stored and prettified

---
+++ web_scraper/web_scraper.py
@@ -0,0 +1,13 @@
+# Scraping from wikipedia page:
+
+import requests as req
+from bs4 import BeautifulSoup as bs
+
+url = 'https://en.wikipedia.org/wiki/UNESCO_Institute_for_Statistics'
+
+response = req.get(url)
+content = response.content
+print(content)
+
+# soup = bs(content,'html.parser' )
+# print(soup.prettify())
